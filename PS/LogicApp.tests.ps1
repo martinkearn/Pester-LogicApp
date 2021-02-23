d49d223bf103487ce724a8c1c91bd2e5ba8ea741
Martin Kearn <martin.kearn@microsoft.com>
2021-02-23 13:30:40 +0000

    Update LogicApp.tests.ps1

--- PS/LogicApp.tests.ps1
+++ PS/LogicApp.tests.ps1
@@ -1,19 +1,32 @@
-# Load .env file environment variables
-if (-not (Get-Module -ListAvailable -Name Set-PsEnv)) {
-    Install-Module -Name Set-PsEnv -Force
-}
-Set-PsEnv
+# Invoke with Invoke-Pester -Output Detailed LogicApp.tests.ps1
 
-# Inlcude the utility files
+# Include the utility file
 . "$PSScriptRoot\GetLogicAppActionResult.ps1"
 
-# Variables
-$resourceGroupName = $Env:RESOURCEGROUPNAME
-$logicAppName = $Env:LOGICAPPNAME
-$logicAppUri = $Env:LOGICAPPURI
-$uniqueId = New-Guid
-
 Describe "Logic App Integration Tests | UniqueId: $uniqueId" {
+
+    BeforeAll {
+        # Load .env file environment variables
+        if (-not (Get-Module -ListAvailable -Name Set-PsEnv)) {
+            Install-Module -Name Set-PsEnv -Force
+        }
+        Set-PsEnv
+
+        # Variables
+        $uniqueId = New-Guid
+    }
+
+    Context "Host machine has the environment variables setup correctly" {
+        It "Expects LOGICAPPURI environment variables to be setup" {
+            $env:LOGICAPPURI | Should -Not -BeNullOrEmpty
+        }
+        It "Expects LOGICAPPNAME environment variables to be setup" {
+            $env:LOGICAPPNAME | Should -Not -BeNullOrEmpty
+        }
+        It "Expects RESOURCEGROUPNAME environment variables to be setup" {
+            $env:RESOURCEGROUPNAME | Should -Not -BeNullOrEmpty
+        }
+    }
 
     Context "Trigger the logic app" {
 
@@ -27,7 +40,7 @@ Describe "Logic App Integration Tests | UniqueId: $uniqueId" {
             "Content-Type"="application/json"
         } 
 
-        $request = Invoke-RestMethod -Method 'POST' -Uri $logicAppUri -Body $postBody -Headers $postHeaders
+        $request = Invoke-RestMethod -Method 'POST' -Uri $env:LOGICAPPURI -Body $postBody -Headers $postHeaders
     }
 
 }
