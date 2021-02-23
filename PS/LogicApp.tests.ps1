77af4ee337c2d98719df7ab497e8f212408c49ef
Martin Kearn <martin.kearn@microsoft.com>
2021-02-23 12:51:14 +0000

    Triggering logic app

--- PS/LogicApp.tests.ps1
+++ PS/LogicApp.tests.ps1
@@ -1,4 +1,33 @@
-$resourceGroupName = "Temp"
-$workflowName = "LogicAppIntTest"
+# Load .env file environment variables
+if (-not (Get-Module -ListAvailable -Name Set-PsEnv)) {
+    Install-Module -Name Set-PsEnv -Force
+}
+Set-PsEnv
 
+# Inlcude the utility files
 . "$PSScriptRoot\GetLogicAppActionResult.ps1"
+
+# Variables
+$resourceGroupName = $Env:RESOURCEGROUPNAME
+$logicAppName = $Env:LOGICAPPNAME
+$logicAppUri = $Env:LOGICAPPURI
+$uniqueId = New-Guid
+
+Describe "Logic App Integration Tests | UniqueId: $uniqueId" {
+
+    Context "Trigger the logic app" {
+
+        $postBody = @{
+            "location"="London"
+            "uniqueid"="$uniqueId";
+        } | ConvertTo-Json
+
+        $postHeaders = @{
+            "Accept"="application/json"
+            "Content-Type"="application/json"
+        } 
+
+        $request = Invoke-RestMethod -Method 'POST' -Uri $logicAppUri -Body $postBody -Headers $postHeaders
+    }
+
+}
