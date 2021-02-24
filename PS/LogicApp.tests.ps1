d42d32eb039681f044261614d3257b152c712c8a
Martin Kearn <martin.kearn@microsoft.com>
2021-02-24 13:09:10 +0000

    Updated placement for Pester v5

--- PS/LogicApp.tests.ps1
+++ PS/LogicApp.tests.ps1
@@ -1,47 +1,33 @@
 # Invoke with Invoke-Pester -Output Detailed LogicApp.tests.ps1
 
-# Include the utility file
-. "$PSScriptRoot\GetLogicAppActionResult.ps1"
+BeforeAll { 
+    # Include the utility file
+    . "$PSScriptRoot\GetLogicAppActionResult.ps1"
+
+    $uniqueId = New-Guid
+}
+
 
 Describe "Logic App Integration Tests | UniqueId: $uniqueId" {
-
-    BeforeAll {
-        # Load .env file environment variables
-        if (-not (Get-Module -ListAvailable -Name Set-PsEnv)) {
-            Install-Module -Name Set-PsEnv -Force
-        }
-        Set-PsEnv
-
-        # Variables
-        $uniqueId = New-Guid
-    }
 
     Context "Host machine has the environment variables setup correctly" {
         It "Expects LOGICAPPURI environment variables to be setup" {
             $env:LOGICAPPURI | Should -Not -BeNullOrEmpty
         }
-        It "Expects LOGICAPPNAME environment variables to be setup" {
-            $env:LOGICAPPNAME | Should -Not -BeNullOrEmpty
-        }
-        It "Expects RESOURCEGROUPNAME environment variables to be setup" {
-            $env:RESOURCEGROUPNAME | Should -Not -BeNullOrEmpty
-        }
     }
 
-    Context "Trigger the logic app" {
+    Context "Trigger the logic app and test the results" {
+        BeforeAll { 
+            $postBody = @{
+                location = "London"
+                uniqueid = $uniqueId
+            } | ConvertTo-Json
 
-        $postBody = @{
-            "location"="London"
-            "uniqueid"="$uniqueId";
-        } | ConvertTo-Json
-
-        $request = Invoke-RestMethod -Method 'POST' -Uri $env:LOGICAPPURI -Body $postBody -ContentType "application/json"
-
-        Write-Host $request
-        Write-Host $request.responses.daily.day | ConvertTo-Json
+            $response = Invoke-WebRequest -Method POST -Uri $env:LOGICAPPURI -Body $postBody -ContentType "application/json"
+        }
 
         It "Post request completed with 200" {
-            $request.StatusCode | Should -Be 200
+            $response.StatusCode | Should -Be 200
         }
     }
 
