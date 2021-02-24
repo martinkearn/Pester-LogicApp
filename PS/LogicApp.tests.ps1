4b0da0a4e0251e3ccaa0ab22f4218017f62f83cb
Martin Kearn <martin.kearn@microsoft.com>
2021-02-24 15:35:50 +0000

    Loading env vars from .env

--- PS/LogicApp.tests.ps1
+++ PS/LogicApp.tests.ps1
@@ -4,17 +4,35 @@ BeforeDiscovery {
     # Include the utility file
     . "$PSScriptRoot\GetLogicAppActionResult.ps1"
 
+    # Load environment variables from local .env file by using Set-PsEnv
+    if (-not (Get-Module -ListAvailable -Name Set-PsEnv)) {
+        Install-Module -Name Set-PsEnv -Force
+    }
+    Set-PsEnv
+
     $uniqueId = New-Guid
 }
 
 Describe "Logic App Integration Tests | UniqueId: $uniqueId" {
+
     Context "Host machine has the environment variables setup correctly" {
-        It "Expects LOGICAPPURI environment variables to be setup" {
+
+        It "Expects LOGICAPPURI environment variable to be setup" {
             $env:LOGICAPPURI | Should -Not -BeNullOrEmpty
         }
+
+        It "Expects LOGICAPPNAME environment variable to be setup" {
+            $env:LOGICAPPNAME | Should -Not -BeNullOrEmpty
+        }
+
+        It "Expects RESOURCEGROUPNAME environment variable to be setup" {
+            $env:RESOURCEGROUPNAME | Should -Not -BeNullOrEmpty
+        }
+
     }
 
     Context "Trigger the logic app and test the results" {
+
         BeforeAll { 
             $postBody = @{
                 location = "London"
@@ -27,6 +45,7 @@ Describe "Logic App Integration Tests | UniqueId: $uniqueId" {
         It "Post request completed with 200" {
             $response.StatusCode | Should -Be 200
         }
+
     }
 
 }
