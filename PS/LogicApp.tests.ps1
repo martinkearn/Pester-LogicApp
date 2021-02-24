e48383165b1821c38ef399a68669402f3ad7fd75
Martin Kearn <martin.kearn@microsoft.com>
2021-02-24 17:15:32 +0000

    Even cleaner code than before

--- PS/LogicApp.tests.ps1
+++ PS/LogicApp.tests.ps1
@@ -39,15 +39,14 @@ Describe "Logic App Integration Tests | UniqueId: $uniqueId" {
                 uniqueid = $uniqueId
             } | ConvertTo-Json
             $response = Invoke-WebRequest -Method POST -Uri $env:LOGICAPPURI -Body $postBody -ContentType "application/json"
-            $locationHeader = $response.Headers.Location
         }
         It "Has sucessfully completed" {
-            if ($locationHeader -ne $null)
+            # If async responses are enabled on the Logic App, we expect an initial 202 with a header called 'location' containing a uri to get final status which will eventually be 200. Otherwise we just expect a 200 response
+            if ($response.Headers.Location -ne $null)
             {
-                # This means that async repsonses are enabled on the Logic App so we expect an initial 202 with a header called 'location' containing a uri to get final status.
+                Write-Host "This Logic App is configured for async responses. It will take longer to get the final response."
                 $response.StatusCode | Should -Be 202
-                # Wait until the uri on the 'location' header eventually returns 200, then overwrite the initial response with the final one.
-                $response = Wait-ForLogicAppToComplete -LogicappUri $locationHeader -TimeoutMinutes 10
+                $response = Wait-ForLogicAppToComplete -LogicappUri $response.Headers.Location -TimeoutMinutes 10
             }
 
             # Both async and non-async should eventually have a 200 status code.
